fadd95a2e1bd2f61a4e85dd9ce0d88d113176699
Penghua <kids5346@gmail.com>
2020-04-08 21:23:08 +0800

    [feat]: build header component import svg icon and use moment to get current date.

---
+++ src/components/header.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="header">
+    <refreshIcon class="refresh"/>
+    <p class="date">{{ currentDate }}</p>
+  </div>
+</template>
+
+<script>
+import moment from 'moment' 
+
+import refreshIcon from '@/assets/img/refresh-icon.svg'
+
+export default {
+  computed: {
+    currentDate() {
+      return moment().format('dddd, MMM D'); 
+    }
+  },
+  components: {
+    refreshIcon
+  }
+}
+</script>
+
+<style lang="scss" scope>
+.header {
+  height: 200px;
+  border-radius: 15px 15px 0 0;
+  background: url('~@/assets/img/bg.jpg') no-repeat 50% 70%;
+  background-size: cover;
+  position: relative;
+  .refresh {
+    position: absolute;
+    width: 28px;
+    height: 28px;
+    right: 20px;
+    top: 20px;
+    transition: all .2s;
+    color: rgba(255, 255, 255, 1);
+    &:hover {
+      transform: rotate(40deg);
+    }
+  }
+  .date {
+    position: absolute;
+    left: 10px;
+    bottom: 10px;
+    font-size: 25px;
+    color: rgba(255, 255, 255, 1);
+    font-weight: bold;
+  }
+}
+</style>
