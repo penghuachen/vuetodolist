b905959daed4b330b55bb21c12c5cc7ffd656cef
Penghua <kids5346@gmail.com>
2020-04-08 21:22:13 +0800

    [feat]: build container component

---
+++ src/views/container.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <todo-header></todo-header>
+  </div>
+</template>
+
+<script>
+import header from '@/components/header.vue'
+
+export default {
+  components: {
+    'todo-header': header
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .container {
+    margin: 0 auto;
+    width: 500px;
+    background-color: rgba(255, 255, 255, 1);
+    border-radius: 15px 15px 0 0;
+  }
+</style>
