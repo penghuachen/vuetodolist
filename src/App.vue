42c62a5ea7350708baa9c4a011023933652c4ad1
Penghua <kids5346@gmail.com>
2020-04-08 21:27:22 +0800

    [feat]: set css style

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,35 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <container-components />
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import container from '@/views/container.vue'
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+export default {
+  components: {
+    'container-components': container
   }
 }
+</script>
+
+<style lang="scss">
+  p {
+    margin: 0;
+  }
+  svg {
+    cursor: pointer;
+  }
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
+  body {
+    height: 100vh;
+    background: rgba(0, 0, 0, 0.1);
+  }
 </style>
