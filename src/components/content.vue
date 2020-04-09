cc9cbabb8401db351c3458c643e5a122d56a5a21
Penghua <kids5346@gmail.com>
2020-04-09 16:11:52 +0800

    [feat]: build content component

---
+++ src/components/content.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="content">
+    <task-status></task-status>
+    <task-content></task-content>
+  </div>
+</template>
+
+<script>
+import taskStatus from '@/components/task-status.vue';
+import taskContent from '@/components/task-content.vue';
+
+export default {
+  components: {
+    'task-status': taskStatus,
+    'task-content': taskContent
+  } 
+}
+</script>
+
+<style lang="scss" scoped>
+.content {
+  position: relative;
+  padding-top: 55px;
+}
+</style>
